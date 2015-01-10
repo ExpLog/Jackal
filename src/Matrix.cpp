3311c554ec42bb931ef6fa6d2c49843a52dceeba
Felipe de Albuquerque Mello Pereira <felipeamp@gmail.com>
2015-01-10 11:33:53 -0200

    Overloaded [] operator, adds rows and columns functions
    Added a "[ " and " ]" padding in the print function (<<).
    
    Renames _lines to _ rows.
    
    Added member functions that read the number of rows and columns of a
    matrix.
    Overloaded the [] operator to receive two size_type numbers i and j and
    returns the entry [i,j] of the Matrix (starting at [0,0]).
    
    Also fixed a few mistakes in the previous code.

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -1,62 +1,104 @@
+#include <iostream>
+#include <stdexcept>
+#include <vector>
 #include "matrix.h"
 
-Matrix::Matrix(const std::vector< std::vector<double> > &matrix) {
+/*
+Initializes a Matrix by copying from a vector of vector of doubles.
+Expects the vector< vector<double> > to be nonempty.
+This constructor can be used to cast.
+*/
+Matrix::Matrix (const std::vector< std::vector<double> > &matrix) {
 	if (matrix.empty()) {
 		throw std::invalid_argument("Matrix: Cannot create an empty matrix.");
 	}
 
-	//used to check if the input matrix has all lines of same size
+	//used to check if the input matrix has all rows of same size
 	std::vector< std::vector<double> >::size_type length = matrix[0].size();
 
-	_lines = matrix.size();
+	_rows = matrix.size();
 	_columns = length;
 
 	//copies the entries of the input into the private data structure
-	for (auto &line : matrix) {
+	for (const auto &line : matrix) {
 		if (line.size() != length) {
-			_lines = 0;
+			_rows = 0;
 			_columns = 0;
 			_matrix = std::vector<double>();
-			throw std::invalid_argument("Matrix: All the lines in a matrix must have the same length.");
+			throw std::invalid_argument("Matrix: All the rows in a matrix must have the same length.");
 		}
-		for (auto &entry : line) {
+		for (const auto &entry : line) {
 			_matrix.push_back(entry);
 		}
 	}
 }
 
-Matrix::Matrix(int line, int column, double defaultValue) {
-	if (line <= 0 || column <= 0) {
-		_lines = 0;
+/*
+Initializes a Matrix of dimensions rows x columns with the defaultValue.
+Expects rows and columns to be positive integers. 
+If no defaultValue is passed to the function, it initializes the matrix
+with 0.0 in all entries.
+*/
+Matrix::Matrix (int rows, int columns, double defaultValue = 0.0) {
+	if (rows <= 0 || columns <= 0) {
+		_rows = 0;
 		_columns = 0;
 		_matrix = std::vector<double>();
-		throw std::invalid_argument("Matrix: both dimensions must be positive.");
+		throw std::invalid_argument("Matrix: Both dimensions must be positive.");
 	}
-	_lines = line;
-	_columns = column;
-	_matrix = std::vector<double>(line*column, defaultValue);
+	_rows = rows;
+	_columns = columns;
+	_matrix = std::vector<double>(rows * columns, defaultValue);
 }
 
-Matrix::Matrix(const Matrix &matrix) : 
-	_lines(matrix._lines),
-	_columns(matrix._columns) {
+/*
+Copy constructor. Makes a deep copy of the input matrix.
+*/
+Matrix::Matrix (const Matrix &matrix) { 
+	_rows( matrix.rows() );
+	_columns( matrix.columns() );
 	_matrix = std::vector<double>(matrix._matrix);
 }
 
+/*
+Overloads << so we can print a matrix.
+*/
 std::ostream& operator<< (std::ostream& os, const Matrix &matrix) {
-	auto itr = matrix._matrix.begin();
-	auto end = matrix._matrix.end();
+	const auto itr = (matrix._matrix).begin();
+	const auto end = (matrix._matrix).end();
 	int j = 0; //counts the columns, so we can know when to end the line
 
-	os << "Matrix " << matrix._lines << "x" << matrix._columns << std::endl;
+	os << "Matrix " << matrix.rows() << "x" << matrix.columns() << std::endl;
 	for (; itr != end; ++itr) {
 		++j;
-		if (j >= matrix._columns) {
+		
+		if ( j == matrix.columns() ) {
 			j = 0;
-			os << *itr << std::endl;
+			os << *itr << " ]" << std::endl << "[ ";
 			continue;		
 		}
 		os << *itr << ", ";
 	}
 	return os;
+}
+
+/*
+Returns the number of rows of the matrix.
+*/
+int Matrix::rows () {
+	return _rows;
+}
+
+/*
+Returns the number of columns of the matrix.
+*/
+int Matrix::columns () {
+	return _columns;
+}
+
+/*
+Overloads [] to acess an element [i,j] of the matrix.
+*/
+double& operator[] (std::vector< std::vector<double> >::size_type i, std::vector<double>::size_type j) {
+	return _matrix[i * _columns + j];
 }
--- src/Matrix.h
+++ src/Matrix.h
@@ -2,40 +2,68 @@
 #define GUARD_Matrix_h
 
 #include <iostream>
-#include <stdexcept>
 #include <vector>
 
 class Matrix {
 public:
 	//constructors
-	/*
-	Initializes a Matrix by copying from a vector of vector of doubles.
-	Expects that the vector<vector<double>> to be nonempty.
-	*/
-	Matrix(const std::vector< std::vector<double> > &matrix);
 
 	/*
-	Initializes a Matrix of dimensions line x column with the defaultValue.
-	Expects line and column to be positive integers
+	Default constructor, not to be used. 
+	If it is, we value initialize the int variables, otherwise they would have garbage in them.
+	This may avoid future bugs.
 	*/
-	Matrix(int line, int column, double defaultValue);
+	Matrix(): _rows = 0, _columns = 0 {}
+
+	/*
+	Initializes a Matrix by copying from a vector of vector of doubles.
+	Expects the vector< vector<double> > to be nonempty.
+	This constructor can be used to cast.
+	*/
+	Matrix (const std::vector< std::vector<double> >&);
+
+	/*
+	Initializes a Matrix of dimensions rows x columns with the defaultValue.
+	Expects rows and columns to be positive integers. 
+	If no defaultValue is passed to the function, it initializes the matrix
+	with 0.0 in all entries.
+	*/
+	Matrix (int rows, int columns, double defaultValue = 0.0);
 
 	/*
 	Copy constructor. Makes a deep copy of the input matrix.
 	*/
-	Matrix(const Matrix &matrix);
+	Matrix (const Matrix&);
 	//end of constructors
 
 	//printing
 	/*
 	Overloads << so we can print a matrix.
 	*/
-	friend std::ostream& operator<< (std::ostream& outs, const Matrix &matrix);
+	friend std::ostream& operator<< (std::ostream&, const Matrix&);
 	//end of printing
+
+	/*
+	Overloads [] to acess an element [i,j] of the matrix.
+	*/
+	double& operator[] (std::vector< std::vector<double> >::size_type, std::vector<double>::size_type);
+
+	/*
+	Returns the number of rows of the matrix.
+	*/
+	int rows ();
+
+	/*
+	Returns the number of columns of the matrix.
+	*/
+	int columns ();
+	
+
+	
 private:
 	//data structures
 	std::vector<double> _matrix;
-	int _lines;
+	int _rows;
 	int _columns;
 };
 
